aa59f16de2769192634704578b69aba650e39423
Aurora0043 <940403army@gmail.com>
2024-11-11 15:34:59 +0800

    v1

--- app/src/main/java/com/example/gesture/MainActivity.kt
+++ app/src/main/java/com/example/gesture/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,6 +14,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.gesture.ui.theme.GestureTheme
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.painterResource
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +33,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             GestureTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    //PointerEvents()
+                    Tap()
                 }
             }
         }
@@ -31,17 +42,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PointerEvents() {
+    var msg by remember { mutableStateOf("作者：陳琬昀") }
+    Column {
+        Text("\n" + msg)
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(Color.Yellow)
+                .pointerInput(Unit) {
+                    awaitPointerEventScope {
+                        while (true) {
+                            val event = awaitPointerEvent()
+                            msg = "${event.type}, ${event.changes.first().position}"
+                        }
+                    }
+                }
+        )
+    }
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    GestureTheme {
-        Greeting("Android")
+fun Tap() {
+    var msg by remember { mutableStateOf("TAP相關手勢實例") }
+    var count by remember { mutableStateOf(0) }
+    Column {
+        Text("\n" + msg+"\n計數："+count.toString())
+        Image(
+            painter = painterResource(id = R.drawable.pu0),
+            contentDescription = "靜宜之美",
+            modifier = Modifier
+                .fillMaxSize()
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onTap = {msg = "後觸發onTap(短按)"
+                                count++},
+                        onDoubleTap = {msg = "雙擊"
+                                      count+=2},
+                        onLongPress = {msg = "長按"
+                                      count+=3},
+                        onPress = {msg = "先觸發onPress(按下)"}
+
+                    )
+
+                }
+        )
     }
 }
